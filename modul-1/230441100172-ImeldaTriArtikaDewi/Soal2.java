a8749477c8397b41881bd41bb7b162e1779a427d
imelda3 <164436360+imelda3@users.noreply.github.com>
2024-03-28 23:04:58 +0700

    230441100172-ImeldaTriArtikaDewi-DeviDwiNovitasari-Modul1

---
+++ modul-1/230441100172-ImeldaTriArtikaDewi/Soal2.java
@@ -0,0 +1,14 @@
+
+package soal2;
+
+
+public class Soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
