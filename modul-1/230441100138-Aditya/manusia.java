b405e1aa1e69553ea015238d60cf78af4b13e1ff
Adiittiiyaa <aditdacosta715@gmail.com>
2024-03-29 12:30:52 +0700

    MODUL1

---
+++ modul-1/230441100138-Aditya/manusia.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas1;
+
+/**
+ *
+ * @author ADITYA
+ */
+public class manusia {
+    //atribut dari adit
+    String nama;
+    int umur;
+    String alamat;
+    //method dari adit
+    void berjalan(){
+        System.out.println("Adit sedang berjalan.");
+    }
+    void berlari(){
+        System.out.println("Adit sedang berlari.");
+    }
+}
