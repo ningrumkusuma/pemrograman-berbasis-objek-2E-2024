fe29ac05564ca4e3d4be34df74aafd2f9697fa20
AchmadChusnulYakin <yakinchusnul49@gmail.com>
2024-03-29 22:45:30 +0700

    230441100025_AchmadChusnulYakin_AsprakKakDwiAnggaVirgiDanianto

---
+++ modul-1/SourceCodeSoal1/src/sourcecodesoal1/manusia.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sourcecodesoal1;
+
+/**
+ *
+ * @author Chusnul Yakin
+ */
+
+public class manusia {
+    // Atribut
+    String nama;
+    int umur;
+    String alamat;
+
+    // Method
+    void berlari() {
+        System.out.println("Objek " + this.nama + " sedang berlari");
+    }
+    
+    void berjalan() {
+        System.out.println("Objek " + this.nama + " sedang berjalan");
+    }
+    
+    void MengerjakanTugas () {
+        System.out.println("Objek " + this.nama + " sedang mengerjakan tugas");
+    }
+    
+    void MembacaBuku () {
+        System.out.println("Objek " + this.nama + " sedang membaca buku");
+    }
+}
