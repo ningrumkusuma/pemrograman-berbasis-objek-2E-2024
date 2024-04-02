ccba770691ab79315bb0f323d5c20313a5d2e69d
Naufal768 <muhammadnaufal4689@gmail.com>
2024-04-02 15:02:19 +0700

    230441100125-MUHAMMADNAUFALWIDIATMOKO

---
+++ modul-2/230441100125-MuhammadNaufalWidiatmoko/Mahasiswa.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas1;
+
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String alamat;
+    private String jurusan;
+
+    public Mahasiswa(String nim, String nama, String alamat, String jurusan) {
+        this.nim = nim;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.jurusan = jurusan;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+}
+
